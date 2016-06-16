9f89f5a118a425ea02ff5d2d968d8fb49256bb0e
Alex Shvid <a@shvid.com>
2016-06-15 23:20:01 -0700

    rename packages and folders

--- src/main/java/com/shvid/m2p/ClassPathDocument.java
+++ src/main/java/com/shvid/m2p/ClassPathDocument.java
@@ -1,5 +1,5 @@
 /*
- *      Copyright (C) 2015 Noorq, Inc.
+ *      Copyright (C) 2015 Alex Shvid
  *
  *   Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
  *   See the License for the specific language governing permissions and
  *   limitations under the License.
  */
-package com.noorq.m2p;
+package com.shvid.m2p;
 
 import java.io.File;
 import java.io.FileWriter;
